8eec5a69c59b27c845fac59dea08b965d8449bf6
Ashim <panghalashim7832@gmail.com>
2025-06-30 20:51:04 +0530

    A

--- pages/05a_back-easing/script.js
+++ pages/05a_back-easing/script.js
@@ -1,1 +1,15 @@
 import gsap from 'gsap';
+
+const tabs = document.querySelectorAll('.tabs');
+const indicator = document.querySelectorAll('.indicator');
+const tabRow = document.querySelectorAll('.tab-row');
+
+const updateIndicator = (target) => {
+  const tabBunds = target.getBoundingClientRect();
+  const rowBounds = tabRow.getBoundingClientRect();
+
+  const width = tabBunds.width;
+  const offset = tabBunds.left - rowBounds.left;
+
+  gsap.to('.indicato', {});
+};
